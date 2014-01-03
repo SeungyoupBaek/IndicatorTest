a5d6507766395346467ae155ab46dbd8456249a8
SeungyoupBaek <bsy8220@naver.com>
2014-01-03 15:42:45 +0900

    액티비티인디케이터 테스트 완료했습니다.
    액티비티인디케이터 테스트 완료했습니다.

--- IndicatorTest/ViewController.m
+++ IndicatorTest/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator1;
+// 코드를 이용해서 작성
+@property (strong, nonatomic) UIActivityIndicatorView *indicator2;
 @end
 
 @implementation ViewController
@@ -17,13 +19,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    _indicator2 = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    _indicator2.hidesWhenStopped = YES;
+    _indicator2.center = CGPointMake(160, 200);
+    [self.view addSubview:_indicator2];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(IBAction)startIndicator :(id)sender{
+    [_indicator1 startAnimating];
+    [_indicator2 performSelector:@selector(startAnimating) withObject:nil afterDelay:0.3];
+}
+-(IBAction)stopIndicator:(id)sender{
+    [_indicator1 stopAnimating];
+    [_indicator2 performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.7];
 }
 
 @end
